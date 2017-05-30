337fb7a381c5d7441c5250f456a8079295644804
Mathieu Debove <debovemathieu@gmail.com>
2017-05-30 10:06:21 +0200

    Set fr keyboard layout

--- provision/local.sh
+++ provision/local.sh
@@ -2,5 +2,4 @@
 # Add your local customisation here, e.g.
 # sudo timedatectl set-timezone Australia/Sydney
 sudo timedatectl set-timezone Europe/Paris
-sudo setxkbmap fr
-sudo loadkeys fr
+sudo sed -i 's/XKBLAYOUT=".*"/XKBLAYOUT="fr"/' /etc/default/keyboard
